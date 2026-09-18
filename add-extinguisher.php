bbe03c8dcaa78d0ab151b066b148f6ab56d74d62
Jaja-D3v <jaredabrera44@gmail.com>
2026-09-18 15:59:21 +0800

    new

---
+++ add-extinguisher.php
@@ -0,0 +1,87 @@
+<?php $pageTitle = "Dashboard"; ?>
+
+<!DOCTYPE html>
+<!--
+* CoreUI - Free Bootstrap Admin Template
+* @version v5.5.0
+* @link https://coreui.io/product/free-bootstrap-admin-template/
+* Copyright (c) 2026 creativeLabs Łukasz Holeczek
+* Licensed under MIT (https://github.com/coreui/coreui-free-bootstrap-admin-template/blob/main/LICENSE)
+-->
+
+<html lang="en">
+<?php include 'header.php'; ?>
+
+<body>
+  <?php include 'side-nav.php'; ?>
+  <div class="wrapper d-flex flex-column min-vh-100">
+    <?php include 'header-nav.php'; ?>
+
+
+    <div class="body flex-grow-1">
+      <div class="container-lg px-4">
+
+        <form class="row g-3">
+          <div class="col-md-6">
+            <label for="inputEmail4" class="form-label">Fire extinguisher code</label>
+            <input type="email" class="form-control" id="inputEmail4">
+          </div>
+          <div class="col-md-6">
+            <label for="inputPassword4" class="form-label">location</label>
+            <input type="password" class="form-control" id="inputPassword4">
+          </div>
+
+          <div class="col-12">
+            <label for="inputAddress2" class="form-label">Address 2</label>
+            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+          </div>
+          <div class="col-md-6">
+            <label for="inputCity" class="form-label">City</label>
+            <input type="text" class="form-control" id="inputCity">
+          </div>
+          <div class="col-md-4">
+            <label for="inputState" class="form-label">State</label>
+            <select id="inputState" class="form-select">
+              <option selected>Choose...</option>
+              <option>...</option>
+            </select>
+          </div>
+          <!-- for date -->
+          <div class="col-md-2">
+            <label for="datePurchased" class="form-label">Date of Purchased</label>
+            <input type="date" class="form-control" id="datePurchased">
+          </div>
+
+          <div class="col-12">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="gridCheck">
+              <label class="form-check-label" for="gridCheck">
+                Check me out
+              </label>
+            </div>
+          </div>
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+
+
+      </div>
+    </div>
+    <?php include 'footer.php'; ?>
+  </div>
+  <!-- CoreUI and necessary plugins-->
+  <script src="vendors/@coreui/coreui/js/coreui.bundle.min.js"></script>
+  <script src="vendors/simplebar/js/simplebar.min.js"></script>
+  <script>
+    const header = document.querySelector("header.header");
+
+    document.addEventListener("scroll", () => {
+      if (header) {
+        header.classList.toggle("shadow-sm", document.documentElement.scrollTop > 0);
+      }
+    });
+  </script>
+</body>
+
+</html>
---
+++ notification.php
@@ -0,0 +1,56 @@
+<?php $pageTitle = "Notification"; ?>
+
+<!DOCTYPE html>
+<!--
+* CoreUI - Free Bootstrap Admin Template
+* @version v5.5.0
+* @link https://coreui.io/product/free-bootstrap-admin-template/
+* Copyright (c) 2026 creativeLabs Łukasz Holeczek
+* Licensed under MIT (https://github.com/coreui/coreui-free-bootstrap-admin-template/blob/main/LICENSE)
+-->
+
+<html lang="en">
+<?php include 'header.php'; ?>
+
+<body>
+  <?php include 'side-nav.php'; ?>
+  <div class="wrapper d-flex flex-column min-vh-100">
+    <?php include 'header-nav.php'; ?>
+
+
+    <div class="body flex-grow-1">
+      <div class="container-lg px-4">
+
+        CONTENT HERE
+        <div class="card">
+          <div class="card-header">
+            Featured
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="edit-user.php" class="btn btn-primary">Edit</a>
+            <a href="delete-user.php" class="btn btn-danger ">delete</a>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+   <?php include 'footer.php'; ?>
+  </div>
+  <!-- CoreUI and necessary plugins-->
+  <script src="vendors/@coreui/coreui/js/coreui.bundle.min.js"></script>
+  <script src="vendors/simplebar/js/simplebar.min.js"></script>
+  <script>
+    const header = document.querySelector("header.header");
+
+    document.addEventListener("scroll", () => {
+      if (header) {
+        header.classList.toggle("shadow-sm", document.documentElement.scrollTop > 0);
+      }
+    });
+  </script>
+</body>
+
+</html>
